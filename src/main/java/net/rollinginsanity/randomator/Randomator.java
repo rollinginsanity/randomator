c3ad42df63d660e82ec96395f96e2c6d178341bc
rollinginsanity <reece.payne@gmail.com>
2017-06-11 13:33:24 +1000

    Basic Structure
    Setting up the basic structure.

---
+++ src/main/java/net/rollinginsanity/randomator/Randomator.java
@@ -0,0 +1,14 @@
+/**
+ * Created by rollinginsanity on 11/6/17.
+ */
+package net.rollinginsanity.randomator
+
+/**
+ * Class that handle creating random names in the NSA style of adjective_noun.
+ */
+public class Randomator {
+
+    public Randomator() {
+
+    }
+}
---
+++ src/main/java/net/rollinginsanity/randomator/RandomatorRun.java
@@ -0,0 +1,10 @@
+/**
+ * Created by rollinginsanity on 11/6/17.
+ */
+package net.rollinginsanity.randomator;
+
+public class RandomatorRun {
+    public static void main(String[] args) {
+        Randomator randomator = new Randomator();
+    }
+}
